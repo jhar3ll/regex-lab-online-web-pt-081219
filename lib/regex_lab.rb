0206d23381f996296e0294c4f84f19629a4005d6
Jharell Chambers <jharell.chambers@selu.edu>
2019-08-18 18:42:54 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,13 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/^[aeiouAEIOU]\w/)
+    true 
+  else 
+    false 
+ end 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+ text.scan(/\bun\w+/)
 end
 
 def words_five_letters_long(text)
